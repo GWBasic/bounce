652cc15e4229966d0862076629e49eebd502f91e
Andrew Rondeau <git@andrewrondeau.com>
2020-11-24 15:47:45 -0500

    Started testing that the server can recover from a dropped socket

--- src/server.rs
+++ src/server.rs
@@ -124,3 +124,72 @@ async fn peek(stream: TcpStream) -> Result<usize, Error> {
     Ok(bytes)
 }
 
+// Note: Tests are error conditions only, happy-path tests will be handled in general integration tests
+#[cfg(test)]
+mod tests {
+    use async_std::net::{IpAddr, Ipv4Addr, Shutdown, TcpListener, SocketAddr};
+    use async_std::prelude::*;
+    use async_std::task;
+    use async_std::task::JoinHandle;
+    use std::io::{Error, ErrorKind};
+
+    use crypto::aes::KeySize;
+
+    use super::*;
+
+    async fn get_adapter_stream_and_server_future() -> (TcpStream, SocketAddr, JoinHandle<Result<(), Error>>) {
+        let key = Key {
+            key: vec![1 as u8, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32],
+            size: KeySize::KeySize256
+        };
+
+        let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 0);
+        let listener = TcpListener::bind(socket_addr).await.unwrap();
+        let adapter_listener = TcpListener::bind(socket_addr).await.unwrap();
+
+        let client_address = listener.local_addr().unwrap();
+        let adapter_address = adapter_listener.local_addr().unwrap();
+
+        drop(listener);
+        drop(adapter_listener);
+
+        let server_future = task::spawn(run_server(client_address.port(), adapter_address.port(), key.clone()));
+
+        // TODO: Need a signal to await on that indicates that the server is ready
+        task::sleep(core::time::Duration::from_secs_f32(0.5)).await;
+
+        let adapter_stream = TcpStream::connect(adapter_address).await.expect("Can not connect to the server");
+
+        authenticate(key, adapter_stream.clone()).await.expect("Can not authenticate client stream");
+
+        (adapter_stream, adapter_address, server_future)
+    }
+
+    #[async_std::test]
+    async fn client_drops_connection() {
+
+        let (adapter_stream, adapter_address, server_future) = get_adapter_stream_and_server_future().await;
+
+        adapter_stream.shutdown(Shutdown::Both).expect("Can not shut down client stream");
+
+        let adapter_stream = TcpStream::connect(adapter_address).await.expect("Can not connect to the server");
+        adapter_stream.shutdown(Shutdown::Both).expect("Can not shut down client stream");
+
+        // TODO: Need a cancelation token to tell the server to shut down
+    }
+
+    #[async_std::test]
+    async fn client_unexpected_write_connection() {
+
+        let (mut adapter_stream, adapter_address, server_future) = get_adapter_stream_and_server_future().await;
+
+        let buf = [0u8];
+        adapter_stream.write_all(&buf).await.expect("Can not write to client stream");
+
+        let adapter_stream = TcpStream::connect(adapter_address).await.expect("Can not connect to the server");
+        adapter_stream.shutdown(Shutdown::Both).expect("Can not shut down client stream");
+
+        // TODO: Need a cancelation token to tell the server to shut down
+    }
+}
+
